065bf44357aa6905489ae479d3452be6604ba462
Krestus <saldivaralonso.m@gmail.com>
2022-12-27 14:23:37 -0300

    comandos git

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 int main  (void)
 {
     printf ("Hola Mundo");
+    //git clon <repo>
+    //git fetch
+    //git pull origin main
+    //git commit -am "mensaje"
+    //git push origin main
+    
 }
